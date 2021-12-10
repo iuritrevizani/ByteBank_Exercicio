828c15220d9c78ab397919e555f4b391869a0e4b
Iuri Trevizani <iurirosa250@gmail.com>
2021-12-10 15:17:24 -0300

    first commit

---
+++ ByteBank_Original/Funcionarios/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Original.Funcionarios
+{
+    public abstract class Funcionario
+    {      
+        public static int TotalDeFUncionarios { get; private set; }
+        public string Nome { get; set; }
+        public string CPF { get; private set; }
+        public double Salario { get; protected set; }       
+
+        public Funcionario(double salario, string cpf)
+        {
+            Console.WriteLine("Criando Funcionário");           
+            CPF = cpf;
+            Salario = salario;
+            TotalDeFUncionarios++;
+        }
+
+        public abstract void AumentarSalario();
+        public abstract double GetBonificacao();
+  
+    }
+}
---
+++ ByteBank_Original/GerenciadorBonificacao.cs
@@ -0,0 +1,22 @@
+﻿using ByteBank_Original.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Original
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonificacao;
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonificacao += funcionario.GetBonificacao();
+        }       
+        public double GetTotalBonificacao()
+        {
+            return _totalBonificacao;
+        }
+    }
+}
